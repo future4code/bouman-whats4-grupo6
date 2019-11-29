d47685eeecb7dcf55e8ac806148447815526eb48
eduardomendesnorberto <eduardomendesnorberto@outlook.com>
2019-11-29 18:04:41 -0300

    Envio Parcial

---
+++ projeto-semana5-whats4/src/components/Usuario/Usuario.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import './Usuario.css';
+
+class Usuario extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state.arrayUserMsg = [
+      {nomeUser: "", textoMensagem: ""}
+      ]
+    };
+
+
+  controleUser = e =>{
+    this.setState({
+      nomeUser: e.target.value
+    });
+  };
+
+  controleMensagem = e =>{
+    this.setState({
+      textoMensagem: e.target.value
+    });
+  };
+
+  aoSalvar = () => {
+    let arrayteste = [...this.state.arrayUserMsg]
+    const novaMsg = {texto: this.state.textoMensagem}
+    arrayteste.push(novaMsg)
+    this.setState({
+          arrayUserMsg: arrayteste
+    }) 
+}
+  render() {
+    return (
+      <div>
+        <div id="Main-container">
+          {this.state.arrayUserMsg.map((mensagem, index, array)=>{
+            return(
+              <div> 
+                  <h4>{mensagem.nomeUser}</h4>
+                  <p>{mensagem.textoMensagem}</p>
+              </div>
+            );
+            })}
+        </div>
+        <div>
+          <input className="Estilo-usuario" placeholder="Usuário" value={this.state.nomeUser} onChange={this.controleUser}/>
+          <input className="Estilo-Mensagem" placeholder="Mensagem" value={this.state.textoMensagem} onChange={this.controleMensagem}/>
+          <button onClick={this.aoSalvar}>Enviar</button>
+        </div>
+      </div>
+     );
+  }
+
+}
+
+
+
+export default Usuario;
